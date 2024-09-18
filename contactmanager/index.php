b31522b9cfd530752b137c9119d4019c2b4a0cd0
Douglas Jasper <doug.jasper@trios.com>
2024-09-18 18:50:32 -0400

    Added Table

--- contactmanager/index.php
+++ contactmanager/index.php
@@ -15,7 +15,28 @@
     <body>
         <?php include("header.php"); ?>
         <main>
-            Under Construction
+            <h2>Contact List</h2>
+            <table>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email Address</th>
+                    <th>Phone Number</th>
+                    <th>&nbsp;</th> <!-- for edit button -->
+                    <th>&nbsp;</th> <!-- for delete button -->
+                </tr>
+                <?php foreach ($contacts as $contact): ?>
+                    <tr>
+                        <td><?php echo $contact['firstName']; ?></td>
+                        <td><?php echo $contact['lastName']; ?></td>
+                        <td><?php echo $contact['emailAddress']; ?></td>
+                        <td><?php echo $contact['phone']; ?></td>
+                        <td></td> <!-- for edit button -->
+                        <td></td> <!-- for delete button -->
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+            <p><a href="add_contact_form.php">Add Contact</a></p>
         </main>
         <?php include("footer.php"); ?>
     </body> 
